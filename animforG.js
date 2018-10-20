ed236683a7f60ab2660aae73275cccf33a74ea61
Божидар <30438650+Bozhidar02@users.noreply.github.com>
2018-10-19 22:55:57 -1100

    Add files via upload

---
+++ animforG.js
@@ -0,0 +1,60 @@
+class Animal {
+  constructor(anm, source, zone) {
+    this.anm = anm;
+    this.sor = source;
+    this.zone = zone;
+  }
+}
+
+var animals = [
+  new Animal("Cheetah", "../imgs/animals/animal1.jpg", 2),
+  new Animal("Red wolf", "../imgs/animals/animal2.jpg", 5),
+  new Animal("Bay cat", "../imgs/animals/animal3.jpg", 1),
+  new Animal("Hose's palm civet", "../imgs/animals/animal4.jpg", 1),
+  new Animal("Black-footed cat", "../imgs/animals/animal5.jpg", 2),
+  new Animal("Grandidier's mongoose", "../imgs/animals/animal6.jpg", 1),
+  new Animal("Southern river otter", "../imgs/animals/animal7.jpg", 1),
+  new Animal("African wild dog ", "../imgs/animals/animal8.jpg", 2),
+  new Animal("Iberian lynx", "../imgs/animals/animal9.jpg", 2),
+  new Animal("Narrow-striped mongoose", "../imgs/animals/animal10.jpg", 1),
+  new Animal("European mink", "../imgs/animals/animal11.jpg", 5)
+  new Animal("Black-footed ferret", "../imgs/animals/animal12.jpg", 4),
+  new Animal("Cozumel raccoon", "../imgs/animals/animal13.jpg", 3),
+  new Animal("Snow leopard", "../imgs/animals/animal14.jpg", 8),
+  new Animal("Island fox", "../imgs/animals/animal15.jpg", 1),
+  new Animal("Polar bear", "../imgs/animals/animal16.jpg", 8),
+  new Animal("Marbled polecat", "../imgs/animals/animal17.jpg", 2),
+  new Animal("Bdeogale", "../imgs/animals/animal18.jpg", 1),
+  new Animal("Addax", "../imgs/animals/animal19.jpg", 3),
+  new Animal("Dibatag", "../imgs/animals/animal20.jpg", 3),
+  new Animal("Barbary sheep", "../imgs/animals/animal21.jpg", 2),
+  new Animal("Calamian deer", "../imgs/animals/animal22.jpg", 1),
+  new Animal("Bawean deer", "../imgs/animals/animal23.jpg", 1),
+  new Animal("Buru babirusa", "../imgs/animals/animal24.jpg", 1),
+  new Animal("Bison", "../imgs/animals/animal25.jpg", 5),
+  new Animal("Marsh deer", "../imgs/animals/animal26.jpg", 1),
+  new Animal("Kouprey", "../imgs/animals/animal27.jpg", 5),
+  new Animal("Lowland anoa", "../imgs/animals/animal28.jpg", 1),
+  new Animal("Mountain anoa", "../imgs/animals/animal29.jpg", 1),
+  new Animal("Wild goat", "../imgs/animals/animal30.jpg", 2),
+  new Animal("Caucasian turs", "../imgs/animals/animal31.jpg", 6),
+  new Animal("Nubian ibex", "../imgs/animals/animal32.jpg", 3),
+  new Animal("Chacoan peccary", "../imgs/animals/animal33.jpg", 1),
+  new Animal("Aders' duiker", "../imgs/animals/animal34.jpg", 2),
+  new Animal("Abbott's duiker", "../imgs/animals/animal35.jpg", 2),
+  new Animal("Barasingha", "../imgs/animals/animal36.jpg", 1),
+  new Animal("Visayan spotted deer", "../imgs/animals/animal37.jpg", 1),
+  new Animal("Persian fallow deer", "../imgs/animals/animal38.jpg", 2),
+  new Animal("Hirola", "../imgs/animals/animal39.jpg", 2),    
+  new Animal("Beira","../imgs/animals/animal40.jpg",2),
+  new Animal("Cuvier's gazelle","../imgs/animals/animal41.jpg",2),
+  new Animal("Dama gazelle","../imgs/animals/animal42.jpg",3),
+  new Animal("Dorcas gazelle","../imgs/animals/animal43.jpg",3),
+  new Animal("Mountain gazelle","../imgs/animals/animal44.jpg",3),
+  new Animal("Rhim gazelle","../imgs/animals/animal45.jpg",3),
+  new Animal("Red-fronted gazelle","../imgs/animals/animal46.jpg",2),
+  new Animal("Speke's gazelle","../imgs/animals/animal47.jpg",2),
+  new Animal("Goitered gazelle","../imgs/animals/animal48.jpg",4),
+  new Animal("Arabian tahr","../imgs/animals/animal49.jpg",3),
+  new Animal("South Andean deer","../imgs/animals/animal50.jpg",4),    
+];
